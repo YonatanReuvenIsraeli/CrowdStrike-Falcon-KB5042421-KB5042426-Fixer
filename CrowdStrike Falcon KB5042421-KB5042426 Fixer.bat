03bb90686416fb643e20840a7c69cd864d984776
Yonatan Israeli <yonatanreuvenisraeli@gmail.com>
2026-01-28 17:50:51 -0500

    Update CrowdStrike Falcon KB5042421-KB5042426 Fixer.bat

--- CrowdStrike Falcon KB5042421-KB5042426 Fixer.bat
+++ CrowdStrike Falcon KB5042421-KB5042426 Fixer.bat
@@ -2,7 +2,7 @@
 title CrowdStrike Falcon KB5042421/KB5042426 Fixer
 setlocal
 echo Program Name: CrowdStrike Falcon KB5042421/KB5042426 Fixer
-echo Version: 3.1.7
+echo Version: 3.1.8
 echo License: GNU General Public License v3.0
 echo Developer: @YonatanReuvenIsraeli
 echo GitHub: https://github.com/YonatanReuvenIsraeli
@@ -173,7 +173,7 @@ goto "WindowsDriveLetterExist"
 
 :"CheckExistWindowsAssign"
 if not exist "%DriveLetterWindows%\Windows" goto "NotWindowsAssign"
-if /i "%DriveLetterWindows%"=="%SystemDrive%" goto "IsOnline"
+if /i "%DriveLetterWindows%"=="%SystemDrive%" goto "IsOnlineAssign"
 if /i "%DiskPart%"=="True" goto "DiskPartDone"
 goto "CrowdStrikeCheck"
 
@@ -202,30 +202,30 @@ echo There has been an error! Press any key to try again.
 pause > nul 2>&1
 goto "NotWindowsAssign"
 
-:"IsOnline"
-if exist "diskpart.txt" goto "DiskPartExistIsOnline"
+:"IsOnlineAssign"
+if exist "diskpart.txt" goto "DiskPartExistIsOnlineAssign"
 echo.
 echo "%DriveLetterWindows%" is an online Windows installation! Removing drive letter "%DriveLetterWindows%" from volume %WindowsVolume%.
 (echo sel vol %WindowsVolume%) > "diskpart.txt"
 (echo remove letter=%DriveLetterWindows%) >> "diskpart.txt"
 (echo exit) >> "diskpart.txt"
 "%windir%\System32\diskpart.exe" /s "diskpart.txt" > nul 2>&1
-if not "%errorlevel%"=="0" goto "IsOnlineError"
+if not "%errorlevel%"=="0" goto "IsOnlineAssignError"
 del "diskpart.txt" /f /q > nul 2>&1
 echo Removed drive letter "%DriveLetterWindows%" from volume %WindowsVolume%. Please try again.
 goto "Volume"
 
-:"DiskPartExistIsOnline"
+:"DiskPartExistIsOnlineAssign"
 set DiskPart=True
 echo Please temporarily rename to something else or temporarily move to another location "diskpart.txt" in order for this batch file to proceed. "diskpart.txt" is not a system file. "diskpart.txt" is located in the folder "%cd%". Press any key to continue when "diskpart.txt" is renamed to something else or moved to another location. This batch file will let you know when you can rename it back to its original name or move it back to its original location.
 pause > nul 2>&1
-goto "IsOnline"
+goto "IsOnlineAssign"
 
-:"IsOnlineError"
+:"IsOnlineAssignError"
 del "diskpart.txt" /f /q > nul 2>&1
 echo There has been an error! Press any key to try again.
 pause > nul 2>&1
-goto "IsOnline"
+goto "IsOnlineAssign"
 
 :"DiskPartDone"
 echo.
@@ -278,6 +278,7 @@ goto "SureDriveLetterWindows"
 :"CheckExistDriveLetterWindows"
 if not exist "%DriveLetterWindows%" goto "DriveLetterWindowsNotExist"
 if not exist "%DriveLetterWindows%\Windows" goto "NotWindows"
+if /i "%DriveLetterWindows%"=="%SystemDrive%" goto "IsOnline"
 goto "CrowdStrikeCheck"
 
 :"DriveLetterWindowsNotExist"
@@ -285,7 +286,11 @@ echo "%DriveLetterWindows%" does not exist! Please try again.
 goto "Volume"
 
 :"NotWindows"
-echo Windows not installed on "%DriveLetterWindows%"!
+echo Windows not installed on "%DriveLetterWindows%"! Please try again.
+goto "Volume"
+
+:"IsOnline"
+echo "%DriveLetterWindows%" is an online Windows installation! Please try again.
 goto "Volume"
 
 :"CrowdStrikeCheck"
